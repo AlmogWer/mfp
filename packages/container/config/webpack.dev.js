569accc9d412c2887070a7e3a31d810d68404d3b
Almog Wertzberger <almogw@checkpoint.com>
2022-03-29 13:34:13 +0300

    shared modules

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,9 @@ const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJson=require('../package.json')
+
+
 
 const devConfig = {
     mode: 'development',
@@ -16,7 +19,9 @@ const devConfig = {
             name: 'container',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-            }
+            },
+            shared: packageJson.dependencies,
+
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
